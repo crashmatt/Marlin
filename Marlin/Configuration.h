6964c12ac3a605e6a39375992debc12586a718fa
crashmatt <uavflightdirector@gmail.com>
2016-02-26 21:56:15 +0100

    U&V endstop inverting configuration added

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -348,11 +348,14 @@ Here are some standard links for getting your machine calibrated:
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 const bool X_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Y_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
 const bool X_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
 const bool Y_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
-const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
+
+const bool U_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
+const bool V_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
+const bool U_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
+const bool V_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
+
 //#define DISABLE_MAX_ENDSTOPS
 //#define DISABLE_MIN_ENDSTOPS
 
